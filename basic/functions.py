e8ed825c3ffaee90e9f255d031f025db93c78188
HeWangchen <hewangchen0727@gmail.com>
2012-08-27 13:01:29 +0800

    add def square

--- basic/functions.py
+++ basic/functions.py
@@ -20,6 +20,28 @@ def fibonacci(x):
     if x == 1 or x == 0 : return 1
     else : return fibonacci(x - 1) + fibonacci(x - 2)
 
+def squareRootBi(x,epsilon):
+    assert x >= 0
+    assert epsilon > 0
+    low  = 0
+    high = x
+    guess = (low + high)/2.0
+    ctr = 1
+    while abs(guess**2 - x) > epsilon and ctr <= 100:
+        if guess**2 < x:
+            low = guess
+        else:
+            high = guess
+        guess = (high + low)/2.0
+        ctr += 1
+    assert ctr <= 100
+    print (ctr,guess)
+    return guess
+
+
+a = squareRootBi(24,0.001)
+print(a)
+
 z = fibonacci(12)
 print(z)
 
